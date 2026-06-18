af648c968db0e9135bcf38300223bb0867ce1355
IARevrerz09 <fikriinsaniazhari380@gmail.com>
2026-06-18 23:40:13 +0700

    Fix namespace in Resources.Designer.cs

--- HDATool/Properties/Resources.Designer.cs
+++ HDATool/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HDATextTool.Properties {
+namespace HDATool.Properties {
     using System;
     
     
